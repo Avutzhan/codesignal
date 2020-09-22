9c94f154b2b011eba9c819c73632401e4e0c128a
Avutzhan Dautov <dautov92@list.ru>
2020-09-22 23:11:35 +0600

    Make Array Consecutive 2 js described

--- intro/edge_of_the_ocean/makeArrayConsecutive2.js
+++ intro/edge_of_the_ocean/makeArrayConsecutive2.js
@@ -1,3 +1,19 @@
 function makeArrayConsecutive2(statues) {
     return Math.max(...statues)-Math.min(...statues)+1-statues.length
 }
+
+//Description 1
+//there is an array with number
+//and you must found how many numbers doesnt exist to make consecutive array where one greater other by 1 like [1, 2, 3, 4, 5]
+//for example given array [6, 2, 3, 8] result 4 because we need 1, 4, 5, 7 to make array consecutive by 1
+// to do this we need
+
+function steps(statues) {
+    let max = Math.max(...statues);
+    let min = Math.min(...statues);
+    let length = statues.length;
+    let result = max - min + 1 - length;
+    return length;
+}
+
+console.log(steps([6, 2, 3, 8]));
