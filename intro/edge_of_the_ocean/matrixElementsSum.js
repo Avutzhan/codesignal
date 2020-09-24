87a5a6ef2193b612b7fc3d91ee8bf12dcc879e1f
Avutzhan Dautov <dautov92@list.ru>
2020-09-24 22:39:19 +0600

    Almost Increasing Sequence

---
+++ intro/edge_of_the_ocean/matrixElementsSum.js
@@ -0,0 +1,18 @@
+function matrixElementsSum(matrix) {
+    let total = 0;
+
+    let bannedIndex = [];
+
+    for (let i = 0; i < matrix.length; i++ ) {
+        for (let j = 0; j < matrix[i].length; j++ ) {
+            if (matrix[i][j] === 0) {
+                bannedIndex.push(j);
+            }
+            else if (bannedIndex.indexOf(j) === -1) {
+                total += matrix[i][j];
+            }
+        }
+    }
+
+    return total;
+}
