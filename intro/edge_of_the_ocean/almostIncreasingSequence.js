84a999cc70a50ee1f6b264f8daaa2690d205e038
Avutzhan Dautov <dautov92@list.ru>
2020-09-26 19:24:39 +0600

    Almost Increasing Sequence with semicolon

--- intro/edge_of_the_ocean/almostIncreasingSequence.js
+++ intro/edge_of_the_ocean/almostIncreasingSequence.js
@@ -1,12 +1,12 @@
 function almostIncreasingSequence(sequence) {
-    var bad=0
+    var bad=0;
     for(var i=1;i<sequence.length;i++) {
         if(sequence[i]<=sequence[i-1]) {
-            bad++
-            if(bad>1) return false
-            if(sequence[i]<=sequence[i-2]&&sequence[i+1]<=sequence[i-1]) return false
+            bad++;
+            if(bad>1) return false;
+            if(sequence[i]<=sequence[i-2]&&sequence[i+1]<=sequence[i-1]) return false;
         }
     }
 
-    return true
+    return true;
 }
