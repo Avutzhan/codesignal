b58100c3ba99e1d2e93c644c74c19d0a07f2e1b5
Avutzhan Dautov <dautov92@list.ru>
2020-09-26 19:48:03 +0600

    Almost Increasing Sequence description added

--- intro/edge_of_the_ocean/almostIncreasingSequence.js
+++ intro/edge_of_the_ocean/almostIncreasingSequence.js
@@ -10,3 +10,18 @@ function almostIncreasingSequence(sequence) {
 
     return true;
 }
+
+// Given a sequence of integers as an array, determine whether it
+// is possible to obtain a strictly increasing sequence by removing
+// no more than one element from the array.
+
+// Solution
+// we will use for loop and iterate from second integer
+// if integer less or equal to previous integer we will check it, if not return true;
+// when we check integers
+// if checks (count variable bad ) more than 1 return false;
+// if integer less or equal to previous through 1 integer and next less or equal to previous then return false;
+// if all checks failed return true;
+
+console.log(almostIncreasingSequence([1, 3, 2, 1])); //false impossible to make sequence
+console.log(almostIncreasingSequence([1, 3, 2])); //true can make if drop 3 result 1, 2
