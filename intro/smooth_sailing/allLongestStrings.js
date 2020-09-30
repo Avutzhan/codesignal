e8a42e065226b4c4614aabbc9e128e348ffae563
Avutzhan Dautov <dautov92@list.ru>
2020-10-01 00:03:13 +0600

    All Longest Strings code added

---
+++ intro/smooth_sailing/allLongestStrings.js
@@ -0,0 +1,20 @@
+function allLongestStrings(inputArray) {
+    var obj = {};
+    var arr = [];
+    var wordLength = 0;
+
+    for (var i = 0; i < inputArray.length; i++) {
+        obj[inputArray[i]] = inputArray[i].length;
+    }
+    for (var keys in obj) {
+        if (obj[keys] >= wordLength) {
+            wordLength = obj[keys];
+        }
+    }
+    for (var word = 0; word < inputArray.length; word++) {
+        if (inputArray[word].length === wordLength) {
+            arr.push(inputArray[word]);
+        }
+    }
+    return arr;
+}
